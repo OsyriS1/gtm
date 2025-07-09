88a1409e70066f23b0f37fefc1cb4e61a67eccd1
OsyriS1 <119109418+OsyriS1@users.noreply.github.com>
2025-07-09 13:04:43 +0700

    Update gtm.js

--- gtm.js
+++ gtm.js
@@ -1,7 +1,11 @@
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-56ZMVDMH');</script>
-<!-- End Google Tag Manager -->
+(function() {
+  if (!window.document.scripts.length) {
+    var noscript = document.createElement('iframe');
+    noscript.src = 'https://www.googletagmanager.com/ns.html?id=GTM-56ZMVDMH';
+    noscript.height = '0';
+    noscript.width = '0';
+    noscript.style.display = 'none';
+    noscript.style.visibility = 'hidden';
+    document.body.appendChild(noscript);
+  }
+})();
